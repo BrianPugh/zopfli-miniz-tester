50acc9b10b2242b27c10ecb5912b8eba06b03b42
Brian Pugh <bnp117@gmail.com>
2021-07-14 18:37:57 -0700

    more complete test

--- Makefile
+++ Makefile
@@ -1,12 +1,5 @@
-TARGET ?= a.out
+TARGET ?= compare
 SRC_DIRS ?= ./src
-
-#PATH=/usr/local/opt/llvm/bin:$PATH
-#CPLUS_INCLUDE_PATH=$(llvm-config --includedir):$CPLUS_INCLUDE_PATH 
-#LD_LIBRARY_PATH=$(llvm-config --libdir):$LD_LIBRARY_PATH 
-
-#CC=/usr/local/opt/llvm/bin/clang
-CC=gcc
 
 SRCS := $(shell find $(SRC_DIRS) -name *.cpp -or -name *.c -or -name *.s)
 OBJS := $(addsuffix .o,$(basename $(SRCS)))
@@ -15,12 +8,8 @@ DEPS := $(OBJS:.o=.d)
 INC_DIRS := $(shell find $(SRC_DIRS) -type d)
 INC_FLAGS := $(addprefix -I,$(INC_DIRS))
 
-CDEFS := -DMINIZ_NO_TIME
-CPPFLAGS ?= $(CDEFS) $(INC_FLAGS) -MMD -MP -fsanitize=address -O1 -fno-omit-frame-pointer -g
-#LDFLAGS="-L/usr/local/opt/llvm@8/lib -Wl,-rpath,/usr/local/opt/llvm@8/lib"
-
-#LDFLAGS += -L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib
-#CPPFLAGS += -I/usr/local/opt/llvm/include -I/usr/local/opt/llvm/include/c++/v1/
+CDEFS := -DMINIZ_NO_TIME -DMINIZ_NO_MALLOC
+CPPFLAGS ?= $(CDEFS) $(INC_FLAGS) -MMD -MP
 
 $(TARGET): $(OBJS)
 	$(CC) $(LDFLAGS) $(OBJS) -o $@ $(LOADLIBES) $(LDLIBS)
--- src/main.c
+++ src/main.c
@@ -2,11 +2,18 @@
 #include <stdint.h>
 #include "miniz.h"
 
+#define MEM_SIZE (32 * 1048576)
 
-uint8_t GB_ROM_COMP[1048576];
+uint8_t GB_ROM[MEM_SIZE];
+uint8_t GB_ROM_COMP[MEM_SIZE];
 
 #define BANK_SIZE 16384
 
+/**
+ * Usage:
+ *     ./compare uncompressed_rom compressed_rom
+ *
+ */
 int main(int argc, char *argv[])
 {
     char fout_buf[256];
@@ -15,9 +22,16 @@ int main(int argc, char *argv[])
     bank = malloc(BANK_SIZE);
     size_t comp_bytes = 0;
 
-    /* Read in compressed rom to memory */
+    /* Read in UNcompressed rom to memory */
     {
         FILE *fp = fopen(argv[1], "r");
+        comp_bytes = fread(GB_ROM, 1, sizeof GB_ROM, fp);
+        fclose(fp);
+    }
+
+    /* Read in compressed rom to memory */
+    {
+        FILE *fp = fopen(argv[2], "r");
         comp_bytes = fread(GB_ROM_COMP, 1, sizeof GB_ROM_COMP, fp);
         fclose(fp);
     }
@@ -39,13 +53,17 @@ int main(int argc, char *argv[])
                 );
         assert(status == TINFL_STATUS_DONE);
         assert(dst_buf_size == BANK_SIZE);
+
+        assert(0 == memcmp(bank, &GB_ROM[n_banks * BANK_SIZE], BANK_SIZE));
+
         src_offset += src_buf_size;
         n_banks += 1;
     }while(src_offset < comp_bytes);
 
+    //bank[16384] = 0xFF;  // testing if valgrind will detect this
     printf("Parsed %d banks\n", n_banks);
-    //free(bank);
-
+    printf("All decoded banks matched.\n");
+    free(bank);
 
     return 0;
 }
